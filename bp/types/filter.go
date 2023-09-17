646c028b9bc47ab361812b6a7a7367d569b60b28
Svdex <ucihaibna@yahoo.com>
2023-09-17 16:16:36 +0700

    Fix filter

--- bp/types/filter.go
+++ bp/types/filter.go
@@ -1,11 +1,15 @@
 package types
 
 type Filter struct {
-	AllOf    []Filter    `json:"all_of,omitempty"` // AND
-	AnyOf    []Filter    `json:"any_of,omitempty"` // OR
-	Test     string      `json:"test"`
-	Subject  string      `json:"subject,omitempty"`
-	Domain   string      `json:"domain,omitempty"`
-	Operator string      `json:"operator,omitempty"`
-	Value    interface{} `json:"value,omitempty"`
+	AllOf    []Filter `json:"all_of,omitempty"` // AND
+	AnyOf    []Filter `json:"any_of,omitempty"` // OR
+	Test     string   `json:"test,omitempty"`
+	Subject  string   `json:"subject,omitempty"`
+	Domain   string   `json:"domain,omitempty"`
+	Operator string   `json:"operator,omitempty"`
+
+	// Check other family
+	OtherWithFamilies string `json:"other_with_families,omitempty"`
+
+	Value interface{} `json:"value,omitempty"`
 }
