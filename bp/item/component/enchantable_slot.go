8a97a6201b514a96cc262506d69aa08921dfe40c
Svdex <ucihaibna@yahoo.com>
2023-09-28 17:06:02 +0700

    Add item slot enum

---
+++ bp/item/component/enchantable_slot.go
@@ -0,0 +1,22 @@
+package component
+
+const (
+	EnchantableSlotArmorFeet   string = "armor_feet"
+	EnchantableSlotArmorLegs   string = "armor_legs"
+	EnchantableSlotShield      string = "shield"
+	EnchantableSlotSword       string = "sword"
+	EnchantableSlotSpear       string = "spear"
+	EnchantableSlotBow         string = "bow"
+	EnchantableSlotAxe         string = "axe"
+	EnchantableSlotHoe         string = "hoe"
+	EnchantableSlotShears      string = "shears"
+	EnchantableSlotCrossbow    string = "crossbow"
+	EnchantableSlotArmorTorso  string = "armor_torso"
+	EnchantableSlotShovel      string = "shovel"
+	EnchantableSlotAll         string = "all"
+	EnchantableSlotElytra      string = "elytra"
+	EnchantableSlotCarrotStick string = "carrot_stick"
+	EnchantableSlotArmorHead   string = "armor_head"
+	EnchantableSlotPickaxe     string = "pickaxe"
+	EnchantableSlotDefault     string = ""
+)
---
+++ bp/item/component/wearable_slot.go
@@ -0,0 +1,17 @@
+package component
+
+const (
+	WearableSlotWeaponMainhand string = "slot.weapon.mainhand"
+	WearableSlotWeaponOffhand  string = "slot.weapon.offhand"
+	WearableSlotArmorHead      string = "slot.armor.head"
+	WearableSlotArmorChest     string = "slot.armor.chest"
+	WearableSlotArmorLegs      string = "slot.armor.legs"
+	WearableSlotArmorFeet      string = "slot.armor.feet"
+	WearableSlotHotbar         string = "slot.hotbar"
+	WearableSlotInventory      string = "slot.inventory"
+	WearableSlotEnderchest     string = "slot.enderchest"
+	WearableSlotSaddle         string = "slot.saddle"
+	WearableSlotArmor          string = "slot.armor"
+	WearableSlotChest          string = "slot.chest"
+	WearableSlotEquippable     string = "slot.equippable"
+)
