6d579b8b7e8859681ff012162343bfc71f7e96ee
Svdex <ucihaibna@yahoo.com>
2024-01-08 19:26:40 +0700

    Fix item lang middleware

--- middleware.go
+++ middleware.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	entityBPComponent "github.com/respectZ/glowstone/bp/entity/component"
+	itemComponent "github.com/respectZ/glowstone/bp/item/component"
 	g_util "github.com/respectZ/glowstone/util"
 )
 
@@ -88,9 +89,17 @@ func item_WriteLang(g *Project) error {
 	for _, v := range bp {
 		// Check to prevent overwriting
 		k := fmt.Sprintf("item.%s.name", v.GetNamespaceIdentifier())
-		if !g.RP.Lang.Has(k) {
-			p := g_util.TitleCase(strings.ReplaceAll(v.GetIdentifier(), "_", " "))
-			g.RP.Lang.Add(k, p)
+		var displayName itemComponent.DisplayName
+		_, err := v.Data.GetComponent(&displayName)
+		if err != nil {
+			// If the item does not have a display name, we will use the identifier as the display name.
+			displayName.Value = fmt.Sprintf("item.%s.name", v.GetNamespaceIdentifier())
+			v.Data.AddComponent(&displayName)
+
+			if !g.RP.Lang.Has(k) {
+				p := g_util.TitleCase(strings.ReplaceAll(v.GetIdentifier(), "_", " "))
+				g.RP.Lang.Add(k, p)
+			}
 		}
 	}
 	return nil
